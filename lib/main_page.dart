1b8142b7d4b8752168a3cd5daef4a7a4c240f6b5
DILSHAN <dilshanfonseka76@gmail.com>
2024-07-09 22:01:54 +0530

    update

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -8,6 +8,14 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  int height = 55;
+  int weight = 70;
+
+  String gender = "";
+
+  late double bmi = calculateBMI(height: height,weight: weight);
+  
   @override
   Widget build(BuildContext context) {
 
@@ -19,22 +27,56 @@ class _MainPageState extends State<MainPage> {
           children: [
             Row(
               children: [
-                Padding( 
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(children: [
-                    Icon(Icons.male, size: 150),
-                    const Text("Male")
-                  ],
+                GestureDetector(
+                  onTap: () {
+                    print("Male");
+                    setState(() {
+                      gender = "M";
+                    });
+                    
+                  },
+
+                  child: Container(
+                    height: 200,
+                    width: 175,
+                    decoration: BoxDecoration(
+                      color:gender =="M"? Colors.orange.withAlpha(150):Colors.orange.withAlpha(50),
+                      borderRadius: BorderRadius.circular(25)
+                  
+                    ),
+                     
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(children: [
+                      Icon(Icons.male, size: 150),
+                      const Text("Male")
+                    ],
+                    ),
                   ),
                 ),
                 Spacer(),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(children: [
-                    Icon(Icons.female,size: 150),
-                   const Text("FeMale")
-                  ],),
-                )
+               GestureDetector(
+                onTap: (){
+                  print("Female");
+                  setState(() {
+                    gender = "F";
+                  });
+                },
+                 child: Container(
+                  height: 200,
+                  width: 175,
+                  decoration: BoxDecoration(
+                    color: gender == "F" ? Colors.orange.withAlpha(150):Colors.orange.withAlpha(50),
+                    borderRadius: BorderRadius.circular(25)
+                  ),
+                    
+                
+                   padding: const EdgeInsets.all(8.0),
+                    child: Column(children: [
+                      Icon(Icons.female,size: 150),
+                     const Text("FeMale")
+                    ],),
+                  ),
+               )
               ],
             ),
          Row(
@@ -42,17 +84,28 @@ class _MainPageState extends State<MainPage> {
                 Padding( 
                   padding: const EdgeInsets.all(8.0),
                   child: Column(children: [
-                    const Text("Height"),
-                    Text("176",style:TextStyle(color: Color.fromARGB(255, 231, 30, 30),fontSize: 50,fontWeight: FontWeight.bold),),
+                    Text("Height"),
+                    Text("$height",style:TextStyle(color: Color.fromARGB(255, 231, 30, 30),fontSize: 50,fontWeight: FontWeight.bold),),
                     Row(
                       children: [
                         FloatingActionButton(
-                          onPressed:null,
+                          onPressed:(){
+                            setState(() {
+                             if(height < 250) height++;
+                             bmi = calculateBMI(height: height, weight: weight);
+                            });
+                          },
                         child: Icon(Icons.add,size: 40,)
                         ),
                         const SizedBox(width: 25),
                         FloatingActionButton(
-                          onPressed:null,
+                          onPressed:() {
+                            setState(() {
+                            
+                            if(height > 50)height--;
+                            bmi = calculateBMI(height: height, weight: weight);
+                            });
+                          },
                         child: Icon(Icons.remove,size: 40,)
                         ),
                       ],
@@ -65,16 +118,26 @@ class _MainPageState extends State<MainPage> {
                   padding: const EdgeInsets.all(8.0),
                   child: Column(children: [
                     const Text("Weight"),
-                    Text("50Kg",style:TextStyle(color: Color.fromARGB(255, 231, 30, 30),fontSize: 50,fontWeight: FontWeight.bold),),
+                    Text("$weight",style:TextStyle(color: Color.fromARGB(255, 231, 30, 30),fontSize: 50,fontWeight: FontWeight.bold),),
                     Row(
                       children: [
                         FloatingActionButton(
-                          onPressed:null,
+                          onPressed:(){
+                            setState(() {
+                             if(weight < 200) weight++;
+                             bmi = calculateBMI(height: height, weight: weight);
+                            });
+                          },
                         child: Icon(Icons.add,size: 40,)
                         ),
                         const SizedBox(width: 25),
                         FloatingActionButton(
-                          onPressed:null,
+                          onPressed:(){
+                            setState(() {
+                              if(weight>0)weight--;
+                              bmi = calculateBMI(height: height, weight: weight);
+                            });
+                          },
                         child: Icon(Icons.remove,size: 40,)
                         ),
                       ],
@@ -84,10 +147,13 @@ class _MainPageState extends State<MainPage> {
                 ),
               ],
             ),
+           const SizedBox(height: 50,),
             Column(
               children: [
                 Text("BMI"),
-                Text("50"),
+                Text("${bmi.toStringAsFixed(2)}",
+                style:TextStyle(color: Color.fromARGB(255, 231, 30, 30),fontSize: 50,fontWeight: FontWeight.bold),),
+                Text(getResult(bmi)),
               ],
             ),
         ],)),
@@ -95,4 +161,24 @@ class _MainPageState extends State<MainPage> {
     );
 
   }
+
+  double calculateBMI({required int height,required int weight}){
+    return (weight/(height*height))*10000;
+
+  }
+
+String getResult(bmiValue){
+  if(bmiValue >= 25){
+    return "Overweight";
+  }else if(bmiValue > 18.5){
+    return "Normal";
+  } else{
+    return "UnderWeight";  
+  }
+
+}
+
+
+
+
 }
